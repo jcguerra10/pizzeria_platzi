141071e7003ab1efa1c1f84cbb6ee83d0e82db9d
jcguerra10 <juancamiloguerratabares@gmail.com>
2024-07-16 09:52:13 -0400

    NativeQueries

--- src/main/java/com/platzi/pizzeria/web/controller/OrderController.java
+++ src/main/java/com/platzi/pizzeria/web/controller/OrderController.java
@@ -2,9 +2,11 @@ package com.platzi.pizzeria.web.controller;
 
 import com.platzi.pizzeria.persistence.entity.OrderEntity;
 import com.platzi.pizzeria.service.OrderService;
+import org.hibernate.query.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +35,10 @@ public class OrderController {
     @GetMapping("/outside")
     public ResponseEntity<List<OrderEntity>> getOutsideOrders() {
         return ResponseEntity.ok(this.orderService.getOutsideOrders());
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<List<OrderEntity>> getOrdersCustomer(@PathVariable String id) {
+        return ResponseEntity.ok(this.orderService.getCustomerOrders(id));
     }
 }
