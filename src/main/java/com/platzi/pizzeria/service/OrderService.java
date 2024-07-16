89498b86e98311a322aad492e5474577e8f75a6e
jcguerra10 <juancamiloguerratabares@gmail.com>
2024-07-15 21:13:52 -0400

    Lazy, Eager and JSONIgnore

---
+++ src/main/java/com/platzi/pizzeria/service/OrderService.java
@@ -0,0 +1,22 @@
+package com.platzi.pizzeria.service;
+
+import com.platzi.pizzeria.persistence.entity.OrderEntity;
+import com.platzi.pizzeria.persistence.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<OrderEntity> getAll () {
+        return this.orderRepository.findAll();
+    }
+}
